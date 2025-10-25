235e0d56af4fae3418d48861075ac6ad54659a80
JhonJerickGuyala <gjhonjherick@gmail.com>
2025-10-25 16:31:57 +0800

    BackendBasics

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+
+try {
+    app.listen(port, ()=>{
+        console.log('Listening to port 3000..');
+    })
+} catch (e) {
+    console.log(e);
+}
+
+app.get('/Jerick', async (request, response) =>{
+    response.status(200).json({message: "Hello, I'm Jerick!"});
+});
